a5c23eed4767f64678317fd9b9972b0bf55c9309
meglamp <megan.lampley@students.fhu.edu>
2024-02-06 12:34:47 -0600

    main.py
    first

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 #REMOVE PASS AND FIX THIS FUNCTION
-def anagram():
-    pass
+def anagram(word1, word2):
+    if len(word1) != 0 and len(word2) != 0:
+        if len(word1) == len(word2):
+            word1 = word1.lower()
+            word2 = word2.lower()
+            for i in range(len(word1)):
+                if word1[i] in word2:
+                    yes = "yay"
+                else:
+                    return(False)
+                    yes = "sad"
+                    break
+                if yes == "yay":
+                    return(True)
+        else:
+            return(False)
+    else:
+        return(False)
+
 
 if __name__ == '__main__':
-    #REMOVE PASS YOUR CODE GOES HERE
-    pass
+    word1 = input()
+    word2 = input()
+    print(anagram(word1,word2))
+
     
